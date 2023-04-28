9f0948c723dc90b0234dd30cf44248d5f280c9c6
Huzaifa Arain <huzaifa.itgroup@gmail.com>
2023-04-28 21:50:06 +0500

    feat: add comments for config keys

--- config/telescope-guzzle-watcher.php
+++ config/telescope-guzzle-watcher.php
@@ -2,6 +2,12 @@
 
 // config for MuhammadHuzaifa/TelescopeGuzzleWatcher
 return [
+    /**
+     * headers mentioned in the except_request_headers will be excluded from the request headers.
+     */
     "except_request_headers" => [],
+    /**
+     * headers mentioned in the except_response_headers will be excluded from the response headers.
+     */
     "except_response_headers" => [],
 ];
